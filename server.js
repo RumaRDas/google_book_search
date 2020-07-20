7485843147ff321fec548d01b88fb8034665d352
Ruma Das <rumaadl16@gmail.com>
2020-07-20 23:14:16 +0930

    adding router in server.js file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const router = require('./routes')
 
 // Define middleware here
 const app = express();
@@ -8,6 +9,14 @@ const PORT = process.env.PORT || 5000;
 // Serve up static assets (usually on heroku)
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+  // Add routes, both API and view
+app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bookdata");
