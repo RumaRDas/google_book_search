d6036b0107dbc1411d109c751b94fb30ff7e7ebb
Ruma Das <rumaadl16@gmail.com>
2020-07-20 22:51:40 +0930

    adding models and server js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Define middleware here
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve up static assets (usually on heroku)
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bookdata");
+
+// Start the API server
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
