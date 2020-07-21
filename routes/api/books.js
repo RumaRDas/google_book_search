b067c2f13726fab40cbbd197effb8a211ab6870a
Ruma Das <rumaadl16@gmail.com>
2020-07-21 23:32:08 +0930

    adding api for findby id

--- routes/api/books.js
+++ routes/api/books.js
@@ -3,15 +3,17 @@ const booksController = require("../../controllers/booksController");
 const { db } = require("../../models/book");
 
 // /api/books/author/wagner
-
 router.get("/title/:title", (req, res) =>{
    req.query = { title: req.params.title}
    booksController.findAll(req, res);
 })
 
+// Matches with "/api/books"
 router.route("/")
-    //for getting all books
     .get(booksController.findAll)
     .post(booksController.create)
-
+    
+// Matches with "/api/books/:id"
+.route("/:id")
+.get(booksController.findById)
 module.exports = router;
