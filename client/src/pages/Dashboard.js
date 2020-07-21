487ccd4583be3df1b620a0649aa48e1bb9a409f0
Ruma Das <rumaadl16@gmail.com>
2020-07-22 00:40:22 +0930

    adding dashboard pages

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Jumbotron from '../components/Jumbotron';
+import {Link} from 'react-router-dom';
+import {Container, Row, Col }from '../components/Grid';
+import {Input} from '../components/List';
+
+const Dashboard = () =>{
+    return (
+        <div>
+            
+        
+        </div>
+    )
+}
+
+export default Dashboard
